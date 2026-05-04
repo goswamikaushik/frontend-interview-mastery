1696a8fd825f538d5c49b2688b5eb83143f1cf2e
goswamikaushik <goswamikaushik2003@gmail.com>
2026-05-04 07:58:05 +0530

    complete job-board question 2

--- job-board/answer.js
+++ job-board/answer.js
@@ -5,8 +5,15 @@ const d = structuredClone(jobBoardData);
 function answer(data) {
   // Q1 — Total number of job listings
   const totalJobs = data.length;
+
+  // Q2 — Companies with no jobs posted
+  const companyWithoutJobs = data
+    .filter((c) => !c.jobs || !c.jobs.length)
+    .map(({ companyId, name }) => ({ companyId, name }));
+
   return {
-    "Q1 — Total number of job listings": totalJobs,
+    // "Q2 — Companies with no jobs posted": companyWithoutJobs,
+    // "Q1 — Total number of job listings": totalJobs,
   };
 }
 
