a0b2a4afb85753bd94329e93091887bb34daa7c8
goswamikaushik <goswamikaushik2003@gmail.com>
2026-06-16 20:47:59 +0530

    integer to roman

---
+++ javascript-logic-problems/n-15-integer-to-roman-numeral.js
@@ -0,0 +1,35 @@
+// N-15 · Integer to Roman Numeral
+// Difficulty: Medium | Asked: 2024, 2025
+
+// Problem: Convert integer to Roman numeral string.
+
+// Input: 1994 Output: "MCMXCIV"
+
+// Input: 3749 Output: "MMMDCCXLIX"
+function integerToRoman(num) {
+  const romanValues = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let roman = "";
+  for (let key in romanValues) {
+    while (num >= romanValues[key]) {
+      roman += key;
+      num -= romanValues[key];
+    }
+  }
+  return roman;
+}
+
+console.log(integerToRoman(1994));
