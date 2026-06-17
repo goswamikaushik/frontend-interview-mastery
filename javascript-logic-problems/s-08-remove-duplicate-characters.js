bb422b7755904c45469ec5a85800315be4e4196c
goswamikaushik <goswamikaushik2003@gmail.com>
2026-06-17 15:00:03 +0530

    remove duplicate characters

---
+++ javascript-logic-problems/s-08-remove-duplicate-characters.js
@@ -0,0 +1,37 @@
+// S-08 · Remove Duplicate Characters
+// Difficulty: Easy | Asked: 2024, 2025
+
+// Problem: Return string with duplicate characters removed. Keep first occurrence.
+
+// Input: "banana" Output: "ban"
+function removeDuplicate(str) {
+  let newStr = "";
+  for (const char of str) {
+    if (!newStr.includes(char)) {
+      newStr += char;
+    }
+  }
+  return newStr;
+}
+// console.log(removeDuplicate("banana"));
+
+// better approach
+function removeDuplicate2(str) {
+  const seen = new Set();
+
+  let newStr = "";
+  for (const char of str) {
+    if (!seen.has(char)) {
+      seen.add(char);
+      result += char;
+    }
+  }
+  return newStr;
+}
+// console.log(removeDuplicate2("banana"));
+
+// better approach
+function removeDuplicate3(str) {
+  return [...new Set(str)].join("");
+}
+console.log(removeDuplicate3("banana"));
