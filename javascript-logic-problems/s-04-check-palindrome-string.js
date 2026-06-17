951f959710ca8c2a8fe08e5527d6f543a039f5c4
goswamikaushik <goswamikaushik2003@gmail.com>
2026-06-17 14:05:29 +0530

    palindrome string

---
+++ javascript-logic-problems/s-04-check-palindrome-string.js
@@ -0,0 +1,44 @@
+// S-04 · Check Palindrome String
+// Difficulty: Easy | Asked: Every year
+
+// Problem: Check if a string reads same forwards and backwards. Ignore case, ignore non-alphanumeric.
+
+// Input: "racecar" Output: true
+
+// Input: "A man a plan a canal Panama" Output: true
+
+// Input: "hello" Output: false
+
+function checkPalindrome(str) {
+  const lowercase = str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("");
+
+  let left = 0;
+  let right = lowercase.length - 1;
+
+  while (left < right) {
+    console.log("---left-->", left);
+    console.log("--right-->", right);
+    console.log("--lowercase[left]-->", lowercase[left]);
+    console.log("--lowercase[right]-->", lowercase[right]);
+
+    if (!(lowercase[left] === lowercase[right])) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+  return true;
+}
+
+console.log(checkPalindrome("racecar"));
+
+function checkPalindromeWithMethod(str) {
+  const lowercase = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  return lowercase.split("").reverse().join("") === lowercase;
+}
+// console.log(checkPalindromeWithMethod("racecar"));
