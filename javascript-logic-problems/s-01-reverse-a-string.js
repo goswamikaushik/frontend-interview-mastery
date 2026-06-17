cc5fe23d7ea482f3c27c336e9fea289b4dd7ec44
goswamikaushik <goswamikaushik2003@gmail.com>
2026-06-17 11:47:55 +0530

    reverse string

---
+++ javascript-logic-problems/s-01-reverse-a-string.js
@@ -0,0 +1,15 @@
+// S-01 · Reverse a String
+// Difficulty: Easy | Asked: Every year
+
+// Problem: Reverse a string WITHOUT using .reverse(). Use a loop.
+
+// Input: "hello" Output: "olleh"
+function reverseString(str) {
+  let reverseStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
+}
+
+console.log(reverseString("hello"));
