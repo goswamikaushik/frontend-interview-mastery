e79140562df69c3da8398e02bebd1ef14856b708
goswamikaushik <goswamikaushik2003@gmail.com>
2026-07-07 16:41:06 +0530

    star pyramid

---
+++ javascript-logic-problems/07-star-pyramid.js
@@ -0,0 +1,57 @@
+/*
+Pattern 7: Star Pyramid
+
+Input:
+N = 5
+
+Output:
+    *
+   ***
+  *****
+ *******
+*********
+
+Time Complexity: O(N²)
+Space Complexity: O(1)
+*/
+
+/*
+Pattern 7: Star Pyramid
+
+Input:
+N = 5
+
+Output:
+    *
+   ***
+  *****
+ *******
+*********
+
+Time Complexity: O(N²)
+Space Complexity: O(1)
+*/
+
+function pattern(n) {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    // space
+    for (let j = 0; j < n - i - 1; j++) {
+      row += `0`;
+    }
+
+    // stars
+    for (let j = 0; j < 2 * i + 1; j++) {
+      row += `*`;
+    }
+
+    // space
+    for (let j = 0; j < n - i - 1; j++) {
+      row += `0`;
+    }
+
+    console.log(row);
+  }
+}
+
+pattern(5);
